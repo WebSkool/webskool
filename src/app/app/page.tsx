dbc279a955cb886a3f4ce695b005bc881a1bbac2
mael098 <juanvillegasmorales@outlook.com>
2023-03-01 21:36:11 -0600

    barra de menu

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -5,8 +5,20 @@ import './styles.css'
 export default async function App() {
     return (
         <main>
-            <nav>
-                <ul>
+            <nav className="menu-navegacion">
+                <ul className="menu">
+                    <li>
+                        <Link href="/app">
+                            {/* <Image
+                                src={pfp.picture.thumbnail}
+                                alt="Picture of the author"
+                                width={50}
+                                height={50}
+                                layout="responsive"
+                            /> */}
+                            Inicio
+                        </Link>
+                    </li>
                     <li>
                         <Link href="/app">
                             {/* <Image
