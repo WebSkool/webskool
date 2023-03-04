92c727febe55809677dfcba355e6cd797e65ac32
mael098 <juanvillegasmorales@outlook.com>
2023-03-04 14:22:17 -0600

    inplementacion de icon grupos

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -5,6 +5,7 @@ import './styles.css'
 import hgar from '../../../public/hogar(1).svg'
 import horas from '../../../public/despertador.svg'
 import User from '../../../public/usuario.svg'
+import grupos from '../../../public/aplicaciones-anadir.svg'
 
 export default async function App() {
     return (
@@ -51,6 +52,22 @@ export default async function App() {
                                     <Image
                                         className="icono"
                                         src={User}
+                                        alt="Picture of the author"
+                                        width={50}
+                                        height={50}
+                                        layout="responsive"
+                                    />
+                                }
+                            </div>
+                        </Link>
+                    </li>
+                    <li className="lista-menu">
+                        <Link href="/app">
+                            <div className="div-icon">
+                                {
+                                    <Image
+                                        className="icono"
+                                        src={grupos}
                                         alt="Picture of the author"
                                         width={50}
                                         height={50}
