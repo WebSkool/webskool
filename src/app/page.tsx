3ca16e8e5cfcba7b4750c65d2cccd82ff8bdc30b
mael098 <juanvillegasmorales@outlook.com>
2023-03-15 17:42:31 -0600

    estilos de landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,75 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import './styles.css'
+
+// estas son las importaciones de las imagenes
 import image from '../../public/imagen.jpg'
+
+// Aqui terminan las importaciones de las imagenes
+
+// estas son las importaciones de la tipografia
+import { Righteous } from 'next/font/google'
+
+const righteous = Righteous({
+    style: ['normal'],
+    weight: '400',
+    subsets: ['latin'],
+})
+// aqui termina la importacion de la tipografia
 
 export default function Home() {
     return (
         <main>
-            <div>
-                {' '}
-                <Image src={image} alt='Picture of the author' width={50} height={50} layout='responsive' />
-                <h1>Lorem ipsum dolor sit amet.</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut nulla autem voluptatibus eum quasi sed
-                    tenetur! Similique quidem sed corrupti qui cumque esse, quas maxime ab quae at magnam voluptates.
-                </p>
-            </div>
+            <nav>
+                <div className="logo-icon">
+                    {/* <Image
+                        src={}
+                        alt="Picture of the author"
+                        width={50}
+                        height={50}
+                        layout="responsive"
+                    /> */}
+                </div>
+                <ul>
+                    <li>inicio</li>
+                    <li>novedades</li>
+                    <li>contacto</li>
+                </ul>
+                <div></div>
+            </nav>
+            <article>
+                <Image
+                    src={image}
+                    alt="Picture of the author"
+                    width={50}
+                    height={50}
+                    layout="responsive"
+                />
+                <div>
+                    <h1 className={righteous.className}>inicio</h1>
+
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero doloribus animi praesentium
+                        consequatur labore ducimus facilis asperiores ea.
+                    </p>
+                </div>
+            </article>
+            <article>
+                <div>
+                    <h1>contacto</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores earum iusto cumque, sed nulla
+                        aperiam recusandae.
+                    </p>
+                </div>
+                <Image
+                    src={image}
+                    alt="Picture of the author"
+                    width={50}
+                    height={50}
+                    layout="responsive"
+                />
+            </article>
         </main>
     )
 }
